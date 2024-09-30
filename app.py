8d9cd0d27871f66cea76be2f6de5a2d1ec3aa4e1
realhardik18 <realhardik18@gmail.com>
2024-09-30 07:36:18 +0530

    added extension download page

--- app.py
+++ app.py
@@ -23,12 +23,15 @@ def results():
     # Pass the data to the results page
     return render_template('results.html', hospital=hospital_name, state=state,link=policy_pdf_link)
 
-@app.route('/ngo')
+@app.route('/NGO')
 def ngo():
     with open('data.json','r',encoding='utf-8') as file:
         data=json.load(file)
     return render_template('ngo.html',data=data)
 
+@app.route('/policy')
+def policy():
+    return render_template('policy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
